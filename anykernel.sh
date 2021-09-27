2b6f987b6e8549d43d3c0dff39ab20a20e9a6821
arnavpuranik <puranikarnav@gmail.com>
2021-09-27 17:25:25 +0000

    AK3: Let's Serum-ify Aroma Installer!
    Signed-off-by: arnavpuranik <puranikarnav@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=KCUFKernel-V1
+kernel.string=Serum Kernel by Arnav
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -40,19 +40,11 @@ xz -d ${home}/Image.xz || abort
 xz -d ${home}/kernel_dtb.xz || abort
 
 # Read value by user selected from aroma prop files
-cpu_overclock=$(aroma_get_value cpu_overclock)
 wiredbtn_altmode=$(aroma_get_value wiredbtn_altmode)
 fake_selenforce=$(aroma_get_value fake_selenforce)
 newcam_blobs=$(aroma_get_value newcam_blobs)
 
-# cpu overclock
-if [ "$cpu_overclock" == "2" ]; then
-	patch_cmdline "androidboot.cpuoverclock" "androidboot.cpuoverclock=1"
-else
-	patch_cmdline "androidboot.cpuoverclock" ""
-fi
-
-# alternate weired button mode
+# alternate wired button mode
 if [ "$wiredbtn_altmode" == "2" ]; then
     patch_cmdline "androidboot.wiredbtnaltmode" "androidboot.wiredbtnaltmode=1"
 else
